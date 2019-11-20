c4709c15742729756dc4bbd46632326089778172
Kuran <kurankumar@gmail.com>
2019-11-20 11:22:24 -0500

    Finished deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  // grab the form  ✅
+  const taskForm = document.querySelector('#create-task-form');
+  console.log(taskForm);
+
+
+  // grab submit from form ✅
+  taskForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log(event);
+
+  // grab dat id bb :^) ✅
+    const taskList = document.querySelector('#tasks');
+    console.log(taskList);
+
+  // put da info in the tasks list ✅
+     const taskDescription = taskForm.querySelector('#new-task-description').value;
+      console.log(taskDescription);
+
+
+  // append the description to the list✅
+
+    const taskCreate = document.createElement("li");
+      taskCreate.innerText = taskDescription;
+      
+    taskList.appendChild(taskCreate);
+
+    //reset the form ✅
+
+    event.target.reset();
+  })
+
 });
